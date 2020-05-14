abf7e841071842f1932ff49a6389d974d59ab258
akv17 <temakep@list.ru>
2020-05-14 03:04:00 +0300

    reimpl lexer

--- lu/lexer.py
+++ lu/lexer.py
@@ -1,16 +1,18 @@
 import re
 from dataclasses import dataclass, field
 from enum import Enum
+from typing import List
 
 from .types import TokenT
 
 
 class TokenSpec(Enum):
     EOL = -1
-    OP = 0
-    LOC = 1
-    NUM = 2
-    IDENT = 3
+    OP = 2
+    LOC = 3
+    NUM = 4
+    IDENT = 5
+    SYNTAX = 6
 
 
 @dataclass
@@ -19,35 +21,74 @@ class Token:
     val: str
 
 
-EOL_REGEXP = re.compile('^\n+$')
-OP_REGEXP = re.compile('SET|DEF|ADD|SUB')
-LOC_REGEXP = re.compile(r'^r[\d]+$')
-NUM_REGEXP = re.compile(r'^[\d]+$')
-IDENT_REGEXP = re.compile('^[a-zA-Z_]+$')
-REGEXP_TO_TSPEC = {
-    EOL_REGEXP: TokenSpec.EOL,
-    OP_REGEXP: TokenSpec.OP,
-    LOC_REGEXP: TokenSpec.LOC,
-    NUM_REGEXP: TokenSpec.NUM,
-    IDENT_REGEXP: TokenSpec.IDENT,
+SYNTAX_TOKENS = {'(', ')', ','}
+TSPEC_REGEXP_MAP = {
+    re.compile('^\n+$'): TokenSpec.EOL,
+    re.compile(f'[{"".join(SYNTAX_TOKENS)}]'): TokenSpec.SYNTAX,
+    re.compile('DEF|RET|CALL|SET|ADD|SUB|MUL|DIV'): TokenSpec.OP,
+    re.compile(r'^r[\d]+$'): TokenSpec.LOC,
+    re.compile(r'^[\d]+$'): TokenSpec.NUM,
+    re.compile('^[a-zA-Z_]+$'): TokenSpec.IDENT,
 }
 
 
-def _parse_token(token: str) -> TokenT:
-    for t_regexp, t_spec in REGEXP_TO_TSPEC.items():
-        match = t_regexp.match(token)
+class Lexer:
 
-        if match is not None:
-            return Token(spec=t_spec, val=token)
+    def __init__(
+        self,
+        syntax_tokens: set,
+        tspec_regexp_map: dict,
+    ):
+        self._syntax_tokens = syntax_tokens
+        self._tspec_regexp_map = tspec_regexp_map
 
-    else:
-        msg = f'invalid token `{token}`.'
-        raise Exception(msg)
+    def _create_token_obj(self, t_str: str):
+        for t_regexp, t_spec in self._tspec_regexp_map.items():
+            match = t_regexp.match(t_str)
+
+            if match is not None:
+                return Token(spec=t_spec, val=t_str)
+
+        else:
+            msg = f'invalid token `{t_str}`.'
+            raise Exception(msg)
+
+    def _parse_word(self, word: str):
+        synt_tokens = self._syntax_tokens
+
+        w_start = 0
+        for ch in word:
+            if ch not in synt_tokens:
+                break
+            w_start += 1
+
+        le_synt_chars = list(word[:w_start])
+        word = word[w_start:]
+
+        w_end = len(word)
+        for ch in word[::-1]:
+            if ch not in synt_tokens:
+                break
+            w_end -= 1
+
+        ri_synt_chars = list(word[w_end:])
+        word = word[:w_end]
+
+        tokens = le_synt_chars + [word] + ri_synt_chars
+        tokens = [self._create_token_obj(t) for t in tokens if t]
+        return tokens
+
+    def __call__(self, src: str):
+        tokens = []
+
+        for ln in src.split('\n'):
+            for word in ln.split(' ') + ['\n']:
+                tokens.extend(self._parse_word(word))
+
+        return tokens
 
 
-def lexer(src: str):
-    return [
-        _parse_token(token)
-        for ln in src.split('\n')
-        for token in ln.split() + ['\n']
-    ]
+lexer = Lexer(
+    syntax_tokens=SYNTAX_TOKENS,
+    tspec_regexp_map=TSPEC_REGEXP_MAP
+)
