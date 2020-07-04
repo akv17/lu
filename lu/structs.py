71dc505f16f95a0d81f8004d12721feab6b483bf
akv17 <temakep@list.ru>
2020-07-04 20:25:41 +0300

    mv Stack

--- lu/structs.py
+++ lu/structs.py
@@ -1,4 +1,35 @@
+from copy import copy
 from dataclasses import dataclass
+
+
+class Stack:
+
+    def __init__(self):
+        self._cont = []
+
+    def __repr__(self):
+        return repr(self._cont)
+
+    @property
+    def elements(self):
+        return copy(self._cont)
+
+    def clear(self):
+        self._cont = []
+
+    def push(self, el):
+        self._cont.append(el)
+
+    def peek(self):
+        return self._cont[-1] if self._cont else None
+
+    def pop(self):
+        return self._cont.pop() if self._cont else None
+
+    def pop_all(self):
+        rv = self.elements
+        self.clear()
+        return rv
 
 
 class TokenSpec:
