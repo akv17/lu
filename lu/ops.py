2bb160213193ec461e29a1ea934b8379c1c0556f
akv17 <temakep@list.ru>
2020-07-05 04:06:35 +0300

    wip: impl SetOp

--- lu/ops.py
+++ lu/ops.py
@@ -1,22 +1,39 @@
-from .core import Op, SigVar
+from .core import Op, VarDef
 from .structs import TokenSpec
 
 
 class SetOp(Op):
     SIG_DEF = [
-        SigVar(specs=[TokenSpec.LOC, TokenSpec.IDENT]),
-        SigVar(specs=[TokenSpec.LOC, TokenSpec.IDENT, TokenSpec.NUM]),
+        VarDef(specs=[TokenSpec.LOC, TokenSpec.IDENT], allow_unset=True),
+        VarDef(specs=[TokenSpec.LOC, TokenSpec.IDENT, TokenSpec.CONST]),
     ]
+
+    def execute(self, ctx):
+        src, dst = self.sig.bind(ctx)
+
+        if src.sig.spec == TokenSpec.LOC:
+            ctx.set_reg(src.sig.val, dst.val)
+
+        elif src.sig.spec == TokenSpec.IDENT:
+            ctx.set_mem(src.sig.val, dst.val)
 
 
 class AddOp(Op):
     SIG_DEF = [
-        SigVar(specs=[TokenSpec.LOC, TokenSpec.IDENT]),
-        SigVar(specs=[TokenSpec.LOC, TokenSpec.IDENT, TokenSpec.NUM]),
+        VarDef(specs=[TokenSpec.LOC, TokenSpec.IDENT]),
+        VarDef(specs=[TokenSpec.LOC, TokenSpec.IDENT, TokenSpec.CONST]),
     ]
 
 
-OPS = {'SET': SetOp, 'ADD': AddOp}
+class CallOp(Op):
+    SIG_DEF = [VarDef(specs=[TokenSpec.IDENT])]
+
+
+OPS = {
+    'SET': SetOp,
+    'ADD': AddOp,
+    'CALL': CallOp
+}
 
 
 __all__ = ['OPS']
