50bd0af90f60a1a3607b9cf7af9ffb2b68d2d5d9
akv17 <temakep@list.ru>
2020-07-05 04:05:13 +0300

    mv Sig obj

--- lu/compilation.py
+++ lu/compilation.py
@@ -1,28 +1,5 @@
-from .core import Op
+from .core import Signature
 from .ops import OPS
-
-
-class Signature:
-
-    def __init__(self, sig_def, args):
-        self._def = sig_def
-        self._args = args
-
-        if self._def is not None:
-            self._assert_sig_def(self._def)
-
-    def _assert_sig_def(self, sig_def):
-        for def_arg, actual_arg in zip(sig_def, self._args):
-            if not def_arg.is_opt and actual_arg is None:
-                msg = f'missing arg {actual_arg} of sig {sig_def}.'
-                raise Exception(msg)
-
-            if actual_arg.spec not in def_arg.specs:
-                msg = f'got invalid arg {actual_arg} of sig {sig_def}.'
-                raise Exception(msg)
-
-    def bind(self, ctx):
-        pass
 
 
 class Compiler:
@@ -49,3 +26,6 @@ class Compiler:
         proc.ops = [self.compile_op(op) for op in proc.ops]
         proc = self._set_sig(proc)
         return proc
+
+    def compile_procs(self, procs):
+        return [self.compile_proc(p) for p in procs]
